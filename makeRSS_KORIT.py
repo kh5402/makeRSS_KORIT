bc7a9f8314a43b3420ddde839aab9e2aaea5a3c8
kh <127534515+kh5402@users.noreply.github.com>
2023-10-17 18:01:55 +0900

    Update makeRSS_KORIT.py

--- makeRSS_KORIT.py
+++ makeRSS_KORIT.py
@@ -3,14 +3,24 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 from datetime import datetime
 import pytz
+import os
 
 def main():
-    rss = ET.Element("rss", version="2.0")
-    channel = ET.SubElement(rss, "channel")
+    output_file = "feed_Korit.xml"
     
-    ET.SubElement(channel, "title").text = "KORIT"
-    ET.SubElement(channel, "description").text = "韓国のIT&スタートアップ業界専門メディア"
-    ET.SubElement(channel, "link").text = "https://www.korit.jp/column"
+    # 既存のRSSフィードを読み込む
+    existing_links = set()
+    if os.path.exists(output_file):
+        tree = ET.parse(output_file)
+        root = tree.getroot()
+        for item in root.findall(".//item/link"):
+            existing_links.add(item.text)
+    else:
+        root = ET.Element("rss", version="2.0")
+        channel = ET.SubElement(root, "channel")
+        ET.SubElement(channel, "title").text = "KORIT"
+        ET.SubElement(channel, "description").text = "韓国のIT&スタートアップ業界専門メディア"
+        ET.SubElement(channel, "link").text = "https://www.korit.jp/column"
 
     for currentPage in range(1, 11):
         url = f"https://www.korit.jp/column?page={currentPage}"
@@ -24,12 +34,18 @@ def main():
             articleContent = match[:end_index]
             
             link = 'https://www.korit.jp' + articleContent.split('<a href="')[1].split('"')[0]
+            
+            # 既存のリンクならスキップ
+            if link in existing_links:
+                continue
+            
             date = articleContent.split('<time class="c-columnList__date" datetime="')[1].split('"')[0]
             title = articleContent.split('<h2 class="c-columnList__title">')[1].split('</h2>')[0].strip()
             
             dateObj = datetime.fromisoformat(date).astimezone(pytz.timezone('GMT'))
             dateStr = dateObj.strftime('%a, %d %b %Y %H:%M:%S %Z')
             
+            channel = root.find("channel")
             item = ET.SubElement(channel, "item")
             ET.SubElement(item, "title").text = title
             ET.SubElement(item, "link").text = link
@@ -37,13 +53,12 @@ def main():
             
         if not found:
             break
-            
-    # ElementTreeオブジェクトをきれいに整形する
-    xml_str = ET.tostring(rss)
+    
+    # 整形して保存
+    xml_str = ET.tostring(root)
     xml_pretty_str = minidom.parseString(xml_str).toprettyxml(indent="  ")
     
-    # 整形したXMLをファイルに保存
-    with open("rss_feed.xml", "w") as f:
+    with open(output_file, "w") as f:
         f.write(xml_pretty_str)
 
 if __name__ == "__main__":
